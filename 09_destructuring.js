cf6f47c13f9e5ce69eb62065632beed8c7fdec83
Andrii Zilnyk <andrii.zilnyk@gmail.com>
2020-12-31 10:04:46 +0200

    changes in readme and destructuring file

--- 09_destructuring.js
+++ 09_destructuring.js
@@ -7,3 +7,14 @@ const user = {
 const { age, male } = user;
 
 console.log(age, male); // 40 female
+
+const newArray = [
+    { id: 1, name: 'Bob' },
+    { id: 2, name: 'Tom' },
+]
+
+const [ user1, user2 ] = newArray;
+
+console.log(user1); // { id: 1, name: 'Bob' }
+
+console.log(user2); // { id: 2, name: 'Tom' }
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # ES6-features
 
 1. Rest operator
+2. Promises
+3. Arrow Functions
+4. Template Literals
+5. Let Const
+6. Default Parameters
+7. Modules
+8. Classes
+9. Destructuring
